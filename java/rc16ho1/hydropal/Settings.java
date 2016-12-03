57d3eb1da013aff8c78a395c9fb5c3b9d4fe435d
FridgeyFridge <FridgeyFridge@users.noreply.github.com>
2016-12-03 11:02:47 +0800

    Basic Framework
    Basic Framework

---
+++ java/rc16ho1/hydropal/Settings.java
@@ -0,0 +1,46 @@
+package rc16ho1.hydropal;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+            }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_settings, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (item.getItemId()) {
+            case R.id.action_done:
+                finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+}
+
+
+
